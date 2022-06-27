8871d8687b896acaa5e2822b9c7d6537a3ec9dca
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-06-27 16:01:44 -0500

    gh pages added about to deploy

--- src/Pages/About/Components/Bio/Bio.style.js
+++ src/Pages/About/Components/Bio/Bio.style.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components'
+import { Certificate, Ribbon } from '@styled-icons/fluentui-system-regular'
+import { Web } from '@styled-icons/foundation/Web'
+
 
 export const BioContainer = styled.div`
     border-top: 2px solid ${({ theme }) => theme.fontColor};
@@ -20,4 +23,41 @@ export const HiddenBio = styled.div`
     display: ${({ moreInfoState }) => moreInfoState ? 'block' : 'none'};
     transition: 2s ease-in-out;
     opacity: ${({ moreInfoState }) => moreInfoState ? '1' : '0'};
+`
+export const InfoBoxes = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+`
+export const EducationContainer = styled.div`
+    border: 2px solid ${({ theme }) => theme.fontColor };
+    border-radius: 5px;
+    width: 40%;
+
+    p {
+        font-size: 14px;
+    }
+
+    @media only screen and (min-width: 666px) {
+        width: 30%;
+    }
+`
+
+export const CertificateIcon = styled(Certificate)`
+    color: ${({ theme  }) => theme.fontColor };
+    height: 24px;    
+`
+
+export const RibbonIcon = styled(Ribbon)`
+    color: ${({ theme  }) => theme.fontColor };
+    height: 24px;   
+`
+
+export const InternetIcon = styled(Web)`
+    color: ${({ theme  }) => theme.fontColor };
+    height: 24px;   
+`
+
+export const CodingEducationContainer = styled(EducationContainer)`
+
 `
--- src/Pages/About/Components/Bio/index.js
+++ src/Pages/About/Components/Bio/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BioContainer, HiddenBio, MoreInfo } from './Bio.style'
+import { BioContainer, HiddenBio, MoreInfo, InfoBoxes, EducationContainer, CertificateIcon, RibbonIcon, InternetIcon, CodingEducationContainer} from './Bio.style'
 
 export default function Bio(){
     const [moreInfoState, setMoreInfoState] = useState(false)
@@ -18,7 +18,20 @@ export default function Bio(){
                 }
             
             <HiddenBio moreInfoState={moreInfoState}>
-            <p>I have spent the past couple of years working under the guidance of Professor Internet and, recently, I have finished a three month bootcamp to blast over some technologies and practices that have helped me code with intent and with a new found love for CSS.</p>
+                <InfoBoxes>
+                    <EducationContainer>
+                        <h3><em>Education</em></h3>
+                        <RibbonIcon />
+                            <p>Berklee College of Music</p>
+                    </EducationContainer>
+                    <CodingEducationContainer>
+                        <h3><em>Coding Education</em></h3>
+                        <CertificateIcon />
+                            <p>UT Coding Bootcamp</p>
+                        <InternetIcon />
+                            <p>Internet University</p>
+                    </CodingEducationContainer>
+                </InfoBoxes>
             <p>Currently, I am creating a React component library that contains highly reusable UI components such as a contact form, navbar, or calendar. The code base for the components are simple and I intend to expand on the features and accumulate a UI library for myself and the internet at large to use. </p>
             <p>It goes without saying that I couldn't be where I am without the support of my loving partner, Sasha. Her love keeps me on my toes and without her I wouldn't have been able to pursue and continue my studies in web development. Our two little furry boys make sure I understand unconditional love and patience and I'd do anything to keep them happy and healthy. </p>
             </HiddenBio>
